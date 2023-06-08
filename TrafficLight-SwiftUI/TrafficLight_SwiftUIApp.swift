55eae2cf438180af2c42d34d20426742ecd2d8ee
Stanislav Sazonov <slavk11@yandex.ru>
2023-06-08 09:33:41 +0300

    Initial Commit

---
+++ TrafficLight-SwiftUI/TrafficLight_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight_SwiftUIApp.swift
+//  TrafficLight-SwiftUI
+//
+//  Created by Сазонов Станислав on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
