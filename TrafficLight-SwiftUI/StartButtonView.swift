4ad52487fedf246569868d6cbbdc15ec76f4c8b7
Stanislav Sazonov <slavk11@yandex.ru>
2023-06-10 12:43:45 +0300

    Created ButtonView

---
+++ TrafficLight-SwiftUI/StartButtonView.swift
@@ -0,0 +1,27 @@
+//
+//  StartButtonView.swift
+//  TrafficLight-SwiftUI
+//
+//  Created by Сазонов Станислав on 09.06.2023.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    var text: String
+    var action: () -> Void
+    
+    var body: some View {
+        Button(action: {
+            action()
+        }) {
+            Text(text)
+                .font(.title)
+                .foregroundColor(.white)
+                .frame(width: 150, height: 40)
+                .overlay(Capsule().stroke(Color.gray,lineWidth: 5))
+                .background(Color.blue)
+                .cornerRadius(20)
+        }
+    }
+}
