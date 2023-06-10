22146dbe3862262c05fe535c4b8b3ee7a901048c
Stanislav Sazonov <slavk11@yandex.ru>
2023-06-10 12:35:34 +0300

    Added logic for switchLights

--- TrafficLight-SwiftUI/ContentView.swift
+++ TrafficLight-SwiftUI/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var buttonText = "Start"
+    @State private var lightColor = Color.gray
+    @State private var lightIsOff = 0.5
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TrafficLightView(color: lightColor == .red ? .red : .red.opacity(lightIsOff))
+            TrafficLightView(color: lightColor == .yellow ? .yellow : .yellow.opacity(lightIsOff))
+            TrafficLightView(color: lightColor == .green ? .green : .green.opacity(lightIsOff))
+            Spacer()
+            StartButtonView(text: buttonText, action: switchLights)
         }
         .padding()
+    }
+    
+    private func switchLights() {
+        switch lightColor {
+        case .red:
+            lightColor = .yellow
+        case .yellow:
+            lightColor = .green
+        default:
+            lightColor = .red
+        }
+        buttonText = (buttonText == "Start") ? "NEXT" : buttonText
     }
 }
 
